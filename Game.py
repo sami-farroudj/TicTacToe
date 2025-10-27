0cb49f758ef403b08f01d090996852b55fe3df81
IV-Saks <sami.farrroudjpro@gmail.com>
2025-10-27 13:15:21 +0100

    add player play request and horizon check

--- Game.py
+++ Game.py
@@ -3,9 +3,10 @@ board = ["-","-","-",
         "-","-","-"]
 
 player_one = "X"
+player_two = "0"
 Winner = None
 GameRunning = True
-
+#creat Game board
 def printBoard(board):
     print( board[0] +" | "+board[1]+" | "+board[2])
     print("----------")
@@ -13,5 +14,36 @@ def printBoard(board):
     print("----------")
     print(board[6] +" | "+board[7]+" | "+board[8])
 
-printBoard(board)
+# ask player one to play 
+def player1Input(board):
+    place= int(input("enter a number 1-9: "))
+    if place <= 1 and place <= 9 and board[place-1] == "-":
+        board[place-1]=player_one
+    else:
+        print("Spot alraedy take try again")
 
+# ask player two to play
+def player2Input(board):
+    place= int(input("enter a number 1-9: "))
+    if place <= 1 and place <= 9 and board[place-1] == "-":
+        board[place-1]=player_two
+    else:
+        print("Spot alraedy take try again")
+
+
+
+#check if horizntable line are winnig
+def horinzontlecheck(board):
+    global Winner
+    if board[0] == board[1] == board[2] and board[2] != "-":
+        Winner = board[0]
+        return True
+    
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        Winner = board[4]
+        return True
+    
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        Winner = board[8]
+        return True
+    
