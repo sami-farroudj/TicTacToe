9454ee96a4ab021707ea86a79d614bbc9c5c256f
IV-Saks <sami.farrroudjpro@gmail.com>
2025-10-27 11:42:47 +0100

    add  game board

---
+++ Game.py
@@ -0,0 +1,17 @@
+board = ["-","-","-",
+        "-","-","-",
+        "-","-","-"]
+
+player_one = "X"
+Winner = None
+GameRunning = True
+
+def printBoard(board):
+    print( board[0] +" | "+board[1]+" | "+board[2])
+    print("----------")
+    print( board[3] +" | "+board[4]+" | "+board[5])
+    print("----------")
+    print(board[6] +" | "+board[7]+" | "+board[8])
+
+printBoard(board)
+
